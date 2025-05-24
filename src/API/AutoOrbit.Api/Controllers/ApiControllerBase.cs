bc06315464a7e48013a72f0f16200b09c23b7a02
Vincent Vaati <vaativm@gmail.com>
2025-05-24 10:26:27 +0300

    feat: added api base controller

---
+++ src/API/AutoOrbit.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using AutoOrbit.Api.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoOrbit.Api.Controllers;
+
+public class ApiControllerBase : ControllerBase
+{
+    [HttpGet]
+    internal IActionResult ProcessError(ApiBaseResponse baseResponse)
+    {
+        return baseResponse switch
+        {
+            ApiNotFoundResponse => NotFound(new ErrorDetails
+            {
+                Message = ((ApiNotFoundResponse)baseResponse).Message,
+                StatusCode = StatusCodes.Status404NotFound
+            }),
+            _ => throw new NotImplementedException()
+        };
+    }
+}
---
+++ src/API/AutoOrbit.Api/Shared/Models/ApiBaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoOrbit.Api.Shared.Models;
+
+public abstract class ApiBaseResponse
+{
+    public bool IsSuccess { get; set; }
+
+    protected ApiBaseResponse(bool isSuccess)
+    {
+        IsSuccess = isSuccess;
+    }
+}
