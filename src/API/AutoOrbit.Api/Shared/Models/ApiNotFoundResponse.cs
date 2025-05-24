19591087d2cf412782a47d93d2d4706efd9aacca
Vincent Vaati <vaativm@gmail.com>
2025-05-24 10:28:23 +0300

    feat: added generic request response types

---
+++ src/API/AutoOrbit.Api/Shared/Models/ApiNotFoundResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoOrbit.Api.Shared.Models;
+
+public class ApiNotFoundResponse : ApiBaseResponse
+{
+    public ApiNotFoundResponse(string message)
+        : base(false)
+    {
+        Message = message;
+    }
+
+    public string Message { get; set; }
+}
