59727d4fbd528a8d0571a6276bc72350202cbd30
Vincent Vaati <vaativm@gmail.com>
2025-05-24 10:24:43 +0300

    feat: added datime provider

---
+++ src/API/AutoOrbit.Api/Application/Abstractions/IDateTimeProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace AutoOrbit.Api.Application.Abstractions;
+
+public interface IDateTimeProvider
+{
+    DateTime UtcNow { get; }
+}
+
---
+++ src/API/AutoOrbit.Api/Infrastructure/Time/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using AutoOrbit.Api.Application.Abstractions;
+
+namespace AutoOrbit.Api.Infrastructure.Time;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
